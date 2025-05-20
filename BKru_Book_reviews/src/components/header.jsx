d6863c18dd0c20c7b1d591c6f0b7e780b959f76c
krudoppler <bkru33@gmail.com>
2025-05-20 08:33:39 -0500

    fixing profil picture

--- BKru_Book_reviews/src/components/header.jsx
+++ BKru_Book_reviews/src/components/header.jsx
@@ -1,9 +1,11 @@
+import profileImg from '../assets/profilepic.png'
+
 export default function Header() {
   return (
     <header className="main-header">
       <div className="header-content">
         <img
-          src="/src/assets/profilepic.png"
+          src= {profileImg}
           className="profile"
           alt="profile-picture"
         />
